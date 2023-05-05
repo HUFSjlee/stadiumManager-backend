d0a1094749d69f01e503ef392cb20d1466c9fb5b
deepsj1012 <deepsj1012@naver.com>
2023-05-05 16:38:26 +0900

    StadiumAdminController 등록,수정,삭제 추가

--- src/main/java/com/sm/backend/module/stadium/presentation/dto/StadiumDto.java
+++ src/main/java/com/sm/backend/module/stadium/presentation/dto/StadiumDto.java
@@ -1,5 +1,7 @@
 package com.sm.backend.module.stadium.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sm.backend.module.stadium.domain.entity.Stadium;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,8 +9,12 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 public class StadiumDto {
+    public StadiumDto() {}
+    public StadiumDto(Stadium stadium) {
+    }
 
     @Getter
     @Builder
@@ -17,17 +23,35 @@ public class StadiumDto {
     public static class CreateRequest {
         @NotNull
         private String name;
-        private String address;
+
         @Min(1)
         private int size;
+
+        private String address;
+
         @Min(1)
         private int minimumPerson;
+
         @Min(1)
         private int maximumPerson;
-        private String description;
-        private boolean parkingAvailable = false;
+
+        private boolean parkingAvailable = true;
+
         private boolean showerAvailable = false;
+
+        private String descripton;
+
+        //@DateTimeFormat
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        private LocalDateTime startTime;
+
+        //@DateTimeFormat
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        private LocalDateTime endTime;
+
         private boolean active = false;
+
+        private boolean del = false;
     }
 
     @Getter
@@ -35,6 +59,53 @@ public class StadiumDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class CreateResponse {
+        private Long id;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateRequest {
+        @NotNull
+        private String name;
+
+        @Min(1)
+        private Integer size;
+
+        private String address;
+
+        @Min(1)
+        private Integer minimumPerson;
+
+        @Min(1)
+        private Integer maximumPerson;
+
+        private Boolean parkingAvailable;
+
+        private Boolean showerAvailable;
+
+        private String descripton;
+
+        //@DateTimeFormat
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        private LocalDateTime startTime;
+
+        //@DateTimeFormat
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        private LocalDateTime endTime;
+
+        private Boolean active;
+
+        private Boolean del;
+
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UpdateResponse {
         private Long id;
     }
 }
