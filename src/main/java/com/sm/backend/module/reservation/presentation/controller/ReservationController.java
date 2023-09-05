bdeafc637c1454918d6a0236004e42526fccd813
deepsj1012 <deepsj1012@naver.com>
2023-09-05 23:41:53 +0900

    fix : swagger 관련 코드 임시 주석 처리

--- src/main/java/com/sm/backend/module/reservation/presentation/controller/ReservationController.java
+++ src/main/java/com/sm/backend/module/reservation/presentation/controller/ReservationController.java
@@ -8,8 +8,8 @@ import com.sm.backend.module.reservation.presentation.dto.ReservationDto;
 import com.sm.backend.module.reservation.domain.service.ReservationService;
 import com.sm.backend.module.stadium.domain.entity.ReservableStadium;
 import com.sm.backend.module.stadium.presentation.dto.StadiumDto;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,7 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@Api(tags = {"예약 API 정보를 제공하는 Controller"})
+//@Api(tags = {"예약 API 정보를 제공하는 Controller"})
 @RequiredArgsConstructor
 @RequestMapping("/reservations")
 @RestController
@@ -26,7 +26,7 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
-    @ApiOperation(value = "예약 메서드")
+    //@ApiOperation(value = "예약 메서드")
     @PostMapping("/reserve")
     public ResponseEntity reserve(@Validated @RequestBody ReservationDto.CreateRequest request) {
         var response = reservationService.reserve(request);
